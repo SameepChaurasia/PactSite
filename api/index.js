202e35aa40b853c1961c8a7e18ff8cf20ed59c32
Sameep <chaurasiasameep26@gmail.com>
2024-12-02 12:06:55 +0530

    index js correction

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-                                                                        import path from 'path';
+import path from 'path';
 dotenv.config();
 
 mongoose
@@ -15,7 +15,7 @@ mongoose
         console.log(err);
     });
 
-                                                                        const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -29,11 +29,11 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
-                                                                    app.use(express.static(path.join(__dirname, 'client/dist')));
+ app.use(express.static(path.join(__dirname, 'client/dist')));
 
-                                                                    app.get('*', (req, res) => {
-                                                                        res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-                                                                      });
+ app.get('*', (req, res) => {
+       res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+   });
 
 
 
